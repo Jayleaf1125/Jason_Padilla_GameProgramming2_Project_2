3bf015b141384e0f7a09ec766cb68cbf7b9bac56
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2025-06-15 21:24:27 -0400

    Enemy has a hitbox

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -1,107 +1,50 @@
 using UnityEngine;
 using System.Collections;
 
-public enum CombatState
-{
-    Idle,
-    Attack1,
-    Attack2, 
-    Attack3, 
-    Defend
-}
-
-
 public class PlayerCombat : MonoBehaviour
 {
-    Rigidbody2D _rb;
-    Vector2 _vectorMovement;
-    PlayerAnimatorManager _playerAnimatorManager;
+    [SerializeField] Transform attackOnePos;
+    public float attackOneRange;
+    public LayerMask enemyLayer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _rb = GetComponent<Rigidbody2D>();
-        _playerAnimatorManager = GetComponent<PlayerAnimatorManager>();
-        SetState(CombatState.Idle);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) 
+
+    }
+
+    public void HandleAttackOne()
+    {
+        Debug.Log("Hello, I'm Attack One");
+        Collider2D enemy = Physics2D.OverlapCircle(attackOnePos.position, attackOneRange, enemyLayer);
+
+        if (enemy == null)
         {
-            SetState(CombatState.Attack1);
+            Debug.Log("Enemy Not Found");
+            return;
         }
 
+        StartCoroutine(Test(enemy));
     }
 
-    void SetState(CombatState s)
+    IEnumerator Test(Collider2D obj)
     {
-        switch (s)
-        {
-            case CombatState.Idle:
-                Idling();
-                break;
-            case CombatState.Attack1:
-                //StartCoroutine(AttackOne());
-                AttackOne();
-                break;
-            case CombatState.Attack2:
-                StartCoroutine(AttackTwo());
-                break;
-            case CombatState.Attack3:
-                StartCoroutine(AttackThree());
-                break;
-            case CombatState.Defend:
-                //StartCoroutine(Dashing());
-                break;
-        }
+        obj.GetComponent<SpriteRenderer>().color = Color.red;
+        yield return new WaitForSeconds(1f);
+        obj.GetComponent<SpriteRenderer>().color = Color.white;
     }
 
-    void Idling()
+    private void OnDrawGizmos()
     {
-         Debug.Log("Combat Idle");
-        _playerAnimatorManager.PlayIdleAnimation();
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackOnePos.position, attackOneRange);
     }
 
-    //IEnumerator AttackOne()
-    //{
-    //    Debug.Log("Attak One Intiated");
-    //    _playerAnimatorManager.PlayAttackOneAnimation();
 
-    //    //if(Input.GetMouseButtonDown(0))
-    //    //{
-    //    //    SetState(CombatState.Attack2);
-    //    //}
-
-    //    yield return new WaitForSeconds(.1f);
-    //    SetState(CombatState.Idle);
-    //}
-
-    void AttackOne()
-    {
-        Debug.Log("Attak One Intiated");
-        _playerAnimatorManager.PlayAttackOneAnimation();
-    }
-
-    IEnumerator AttackTwo()
-    {
-        Debug.Log("Attak Two Intiated");
-        _playerAnimatorManager.PlayAttackTwoAnimation();
-
-        if (Input.GetMouseButtonDown(0)) { 
-            SetState(CombatState.Attack3); 
-        }
-
-        yield return new WaitForSeconds(3f);
-        SetState(CombatState.Idle);
-    }
-
-    IEnumerator AttackThree()
-    {
-        Debug.Log("Attak Three Intiated");
-        _playerAnimatorManager.PlayAttackThreeAnimation();
-        yield return new WaitForSeconds(0.5f);
-            SetState(CombatState.Idle); 
-    }
 }
